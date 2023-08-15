608382484853f8d5ca8ea3aa05a4e41274bf0e2d
jayaprakash-25 <20311A04R1@sreenidhi.edu.in>
2023-08-15 20:15:41 +0530

    python files

---
+++ Packages/packages.py
@@ -0,0 +1,104 @@
+import math
+sqrt_result = math.sqrt(25)
+print("Square root:", sqrt_result)
+
+import math
+angle = math.radians(45)  
+sine = math.sin(angle)
+cosine = math.cos(angle)
+print("Sine:", sine)
+print("Cosine:", cosine)
+
+
+import math
+factorial = math.factorial(5)
+print("Factorial:", factorial)
+
+import random
+random_int = random.randint(1, 10)
+print("Random integer:", random_int)
+
+import random
+random_float = random.random()
+print("Random float:", random_float)
+
+import random
+choices = ["apple", "banana", "cherry", "date"]
+random_choice = random.choice(choices)
+print("Random choice:", random_choice)
+
+
+import math
+radius = 5
+area = math.pi * radius**2
+print("Area of circle:", area)
+
+
+import datetime
+date1 = datetime.datetime(2023, 1, 1)
+date2 = datetime.datetime(2023, 8, 15)
+time_difference = date2 - date1
+print("Time difference:", time_difference)
+
+
+import random
+
+numbers = [1, 2, 3, 4, 5]
+random.shuffle(numbers)
+print("Shuffled numbers:", numbers)
+
+
+
+
+import random
+
+sample_size = 3
+random_sample = random.sample(range(1, 11), sample_size)
+print("Random sample:", random_sample)
+
+
+import random
+
+random_boolean = random.choice([True, False])
+print("Random boolean:", random_boolean)
+
+
+
+
+
+
+
+
+
+
+import random
+
+numbers = [1, 2, 3, 4, 5]
+random_permutation = random.sample(numbers, len(numbers))
+print("Random permutation:", random_permutation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
